2d5b2e99eed0cb3600d2beb4b18b7704c653b99b
anvie <obin.mf@gmail.com>
2021-04-21 22:13:03 +0700

    update script

--- etc/script/deploy.sh
+++ etc/script/deploy.sh
@@ -3,6 +3,6 @@
 
 
 
-rsync -avzrhcP -e 'ssh -i ~/.ssh/id_ecdsa_sec' --delete-after ./packages/apps/build/ root@149.129.246.35:/home/www/nuchain
+rsync -avzrhcP -e 'ssh -i ~/.ssh/id_ecdsa_sec' --delete-after ./packages/apps/build/ root@$DEPLOY_SERVER:/home/www/nuchain
 
 
