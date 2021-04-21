731f87cccb18d7ebf87d0bb17e0ae9aae251eeb8
anvie <obin.mf@gmail.com>
2021-04-21 22:52:36 +0700

    add custom types for Nuchain Testnet

--- packages/apps-config/src/api/chain/index.ts
+++ packages/apps-config/src/api/chain/index.ts
@@ -1,12 +1,11 @@
 // Copyright 2017-2021 @polkadot/apps-config authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import Phala from './phala';
 import Nuchain from './nuchain';
 
 // alphabetical, based on the actual displayed name
 export default {
-  'Phala PoC-3': Phala,
   'Nuchain': Nuchain,
+  'Nuchain Testnet': Nuchain,
   'Development': Nuchain,
 };
