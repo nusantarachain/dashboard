aae5f7e263e6960e802de71b9887ea4c112f5be2
robin <robin@ansvia.com>
2021-04-22 17:08:33 +0700

    Added custom types for supply chain

--- packages/apps-config/src/api/chain/nuchain.ts
+++ packages/apps-config/src/api/chain/nuchain.ts
@@ -29,5 +29,74 @@ export default {
       "expired": "Moment",
       "revoked": "bool",
       "additional_data": "Option<Vec<u8>>"
-  }
+  },
+    "PropName": "Vec<u8>",
+    "PropValue": "Vec<u8>",
+    "ProductProperty": {
+        "name": "PropName",
+        "value": "PropValue"
+    },
+    "ProductId": "Vec<u8>",
+    "Product": {
+        "id": "ProductId",
+        "owner": "AccountId",
+        "props": "Option<Vec<ProductProperty>>",
+        "registered": "Moment"
+    },
+"Identifier": "Vec<u8>",
+    "Decimal": "i32",
+    "TrackingId": "Identifier",
+    "TrackingEventIndex": "u128",
+    "DeviceId": "Identifier",
+    "Year": "u32",
+    "TrackingStatus": {
+        "_enum": [
+            "Pending",
+            "InTransit",
+            "Delivered"
+        ]
+    },
+    "Track": {
+        "id": "TrackingId",
+        "owner": "AccountId",
+        "status": "TrackingStatus",
+        "products": "Vec<ProductId>",
+        "registered": "Moment",
+        "delivered": "Option<Moment>"
+    },
+    "TrackingEventType": {
+        "_enum": [
+            "TrackingRegistration",
+            "TrackingPickup",
+            "TrackingScan",
+            "TrackingDeliver"
+        ]
+    },
+    "TrackingEvent": {
+        "event_type": "TrackingEventType",
+        "shipment_id": "TrackingId",
+        "location": "Option<ReadPoint>",
+        "readings": "Vec<Reading<Moment>>",
+        "timestamp": "Moment"
+    },
+    "ReadPoint": {
+        "latitude": "Decimal",
+        "longitude": "Decimal"
+    },
+    "ReadingType": {
+        "_enum": [
+            "Humidity",
+            "Pressure",
+            "Shock",
+            "Tilt",
+            "Temperature",
+            "Vibration"
+        ]
+    },
+    "Reading": {
+        "device_id": "DeviceId",
+        "reading_type": "ReadingType",
+        "timestamp": "Moment",
+        "value": "Decimal"
+    }
 };
