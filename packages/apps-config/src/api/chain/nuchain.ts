dd10cd224ddcdafa1518eebdb3572d876be071a9
robin <robin@ansvia.com>
2021-08-28 18:57:59 +0700

    Add custom did types: Attribute and AttributeTransaction

--- packages/apps-config/src/api/chain/nuchain.ts
+++ packages/apps-config/src/api/chain/nuchain.ts
@@ -89,5 +89,20 @@ export default {
     reading_type: 'ReadingType',
     timestamp: 'Moment',
     value: 'Decimal'
+  },
+  Attribute: {
+    name: 'Text',
+    value: 'Text',
+    validity: 'BlockNumber',
+    creation: 'Moment',
+    nonce: 'u64'
+  },
+  AttributeTransaction: {
+    signature: 'Signature',
+    name: 'Text',
+    value: 'Text',
+    validity: 'u32',
+    signer: 'AccountId',
+    identity: 'AccountId'
   }
 };
